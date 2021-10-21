dc17efac87db60f3dd7fb03f08edb15d71e3e138
Jnrsloth <jnrsloth@gmail.com>
2021-10-21 12:14:49 +1100

    Moved Item rendering in hand onto its own layer. This has stopped the RenderLayers between the clothing rendering and the item rendering clashing. which was causing the sleeveless issue. Because renderRecursively is not a method in GeoLayerRenderer, a manual one was specified and taken directly from McpeCommander's repo https://github.com/mcpecommander/Mobultion-mod/blob/acb1f3ddc61b2c2f99b5a0fb8d6d527d2008ca36/src/main/java/dev/mcpecommander/mobultion/entities/endermen/layers/WanderingEndermanHoldingLayer.java#L22. Thanks Commander!

--- src/main/java/software/supernaturali/example/client/renderer/entity/VillagerEntityRenderer.java
+++ src/main/java/software/supernaturali/example/client/renderer/entity/VillagerEntityRenderer.java
@@ -21,49 +21,14 @@ import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 import software.supernaturali.example.client.model.entity.ReplacedVillagerModel;
 
-
 @Environment(EnvType.CLIENT)
 public class VillagerEntityRenderer<T extends MerchantEntity & IAnimatable & VillagerDataContainer> extends GeoEntityRenderer<T> {
-    private VertexConsumerProvider rtb;
-    private Identifier whTexture;
-    private ItemStack itemStack;
 
     public VillagerEntityRenderer(Context ctx) {
         super(ctx, new ReplacedVillagerModel<>());
 //        this.addFeature(new HeadFeatureRenderer(this, ctx.getModelLoader()));
         this.addLayer(new VillagerClothingFeatureRenderer<>(this, "villager"));
-//        this.addLayer(new VillagerHeldItemFeatureRenderer(this)); //replaced by renderRecursively in this class.
-    }
-
-    @Override
-    public void renderEarly(T entity, MatrixStack stackIn, float ticks, VertexConsumerProvider renderTypeBuffer, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float partialTicks) {
-        //store the Render Type Buffer and current texture, we'll need them later.
-        this.rtb = renderTypeBuffer;
-        this.whTexture = this.getTextureLocation(entity);
-        this.itemStack = entity.getEquippedStack(EquipmentSlot.MAINHAND);
-        super.renderEarly(entity, stackIn, ticks, renderTypeBuffer, bufferIn, packedLightIn, packedOverlayIn, red,
-                          green, blue, partialTicks);
-    }
-
-    @Override
-    //This will help render any items in the villagers' hands which will appear when they want to trade.
-    public void renderRecursively(GeoBone bone, MatrixStack stack, VertexConsumer bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-        if (bone.getName().equals("handRight")) { // handRight is the name of the bone you to set the item to attach too. Please see Note
-            stack.push(); //FIXME not sure of the purpose of this and stack.pop, seems to work fine without it.
-            //You'll need to play around with these to get item to render in the correct orientation
-            stack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(180));
-            stack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
-            stack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(85));
-            //You'll need to play around with this to render the item in the correct spot.
-            stack.translate(-0.525D, -0.5D, 0.11D);//(-0.4D, -0.5D, 0.2D)
-            //Sets the scaling of the item.
-            stack.scale(0.9f, 0.9f, 0.9f);//(0.9f, 0.9f, 0.9f)
-            // Change mainHand to predefined Itemstack and Mode to what transform you would want to use.
-            MinecraftClient.getInstance().getItemRenderer().renderItem(this.itemStack, Mode.GROUND, packedLightIn, packedOverlayIn, stack, this.rtb, 0);
-            stack.pop();
-            bufferIn = rtb.getBuffer(RenderLayer.getEntityTranslucent(whTexture));
-        }
-        super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+        this.addLayer(new VillagerHeldItemFeatureRenderer<>(this));
     }
 
     //TODO check what baby villagers look like, does this need to be used? if so where?
@@ -75,7 +40,6 @@ public class VillagerEntityRenderer<T extends MerchantEntity & IAnimatable & Vil
         } else {
             this.shadowRadius = 0.5F;
         }
-
         matrixStack.scale(g, g, g);
     }
 }
--- src/main/java/software/supernaturali/example/client/renderer/entity/VillagerHeldItemFeatureRenderer.java
+++ src/main/java/software/supernaturali/example/client/renderer/entity/VillagerHeldItemFeatureRenderer.java
@@ -2,36 +2,81 @@ package software.supernaturali.example.client.renderer.entity;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.render.model.json.ModelTransformation.Mode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Vec3f;
 import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
+import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
+import software.bernie.geckolib3.util.RenderUtils;
+import software.supernaturali.example.LimberVillMod;
 
-//THIS CLASS IS NO LONGER IN USE
 public class VillagerHeldItemFeatureRenderer <T extends LivingEntity & IAnimatable, M extends AnimatedGeoModel<T>> extends GeoLayerRenderer<T> {
 
-    final IGeoRenderer ctx;
+    private static final Identifier VILLAGER_MODEL = new Identifier(LimberVillMod.ModID, "geo/villager.geo.json");
+    private ItemStack mainHand;
 
-    public VillagerHeldItemFeatureRenderer(IGeoRenderer<T> context) {
-        super(context);
-        this.ctx = context;
+    public VillagerHeldItemFeatureRenderer(IGeoRenderer<T> entityRendererIn) {
+        super(entityRendererIn);
     }
 
-    public void render(MatrixStack stack, VertexConsumerProvider vertexConsumerProvider, int packedLightIn, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+    @Override
+    public void render(MatrixStack matrix, VertexConsumerProvider renderBuffer, int packedLight, T entity,
+                       float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        this.mainHand = entity.getEquippedStack(EquipmentSlot.MAINHAND);
+        GeoModel model = this.getEntityModel().getModel(VILLAGER_MODEL);
+        //Start a manual renderRecursively.
+        for (GeoBone group : model.topLevelBones) {
+            renderRecursively(group, matrix, renderBuffer, packedLight, GeoEntityRenderer.getPackedOverlay(entity, 0));
+        }
+    }
+
+    //Copied from IGeoRenderer but removed the actual rendering of cubes and only render items to prevent overriding
+    //the original renderRecursively and affecting other layers that might need different RenderTypes.
+    //While it might seem like a niche use, it is quite important for example with entities that have both glowing parts
+    //and hold/equip items/armour like endermen. //Taken verbatim from McpeCommander, this is his fix for the issue. Thanks!
+    private void renderRecursively(GeoBone bone, MatrixStack stack, VertexConsumerProvider renderBuffer, int packedLightIn, int packedOverlayIn) {
         stack.push();
-        stack.translate(0.0D, 1.2D, -0.4000000059604645D); //-0.4000000059604645D
-        stack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(0));
-        stack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
-        stack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(0));
-        stack.scale(0.7F, 0.7F, 0.7F);
-        ItemStack itemStack = livingEntity.getEquippedStack(EquipmentSlot.MAINHAND);
-        MinecraftClient.getInstance().getHeldItemRenderer().renderItem(livingEntity, itemStack, Mode.THIRD_PERSON_RIGHT_HAND, false, stack, vertexConsumerProvider, packedLightIn);
+        RenderUtils.translate(bone, stack);
+        RenderUtils.moveToPivot(bone, stack);
+        RenderUtils.rotate(bone, stack);
+        RenderUtils.scale(bone, stack);
+        RenderUtils.moveBackFromPivot(bone, stack);
+        if (bone.getName().equals("handRight")) {
+            stack.push();
+            //You'll need to play around with these to get item to render in the correct orientation
+            stack.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(180));
+            stack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
+            stack.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(85));
+            //You'll need to play around with this to render the item in the correct spot.
+            stack.translate(-0.525D, -0.5D, 0.11D);//(-0.4D, -0.5D, 0.2D)
+            //Sets the scaling of the item.
+            stack.scale(0.9f, 0.9f, 0.9f);//(0.9f, 0.9f, 0.9f)
+            // Change mainHand to predefined Itemstack and TransformType to what transform you would want to use.
+            MinecraftClient.getInstance().getItemRenderer().renderItem(mainHand, ModelTransformation.Mode.THIRD_PERSON_RIGHT_HAND,
+                    packedLightIn, packedOverlayIn, stack, renderBuffer, 0);
+            //Two pops to empty out the MatrixStack.
+            stack.pop();
+            stack.pop();
+            //Stops unnecessary further recursive method calling. Only works if I am rendering one thing per layer.
+            return;
+        }
+        //Continues the renderRecursively with the child bone, has to be done manually as this is not an overridden
+        // method and so can't rely on super methods.
+        if (!bone.isHidden) {
+            for (GeoBone childBone : bone.childBones) {
+                renderRecursively(childBone, stack, renderBuffer, packedLightIn, packedOverlayIn);
+            }
+        }
         stack.pop();
     }
 }
